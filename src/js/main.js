ebc35a41a261cf9fee65267fe9eebb3d14f59f7e
Zach Shewmaker <zach@azaia.net>
2016-10-14 17:44:58 -0700

    Adding various panels. Expanding JS.

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,28 @@
 (function($){
 
+    var $ui = uiFactory();
+
+    function handleStartNewGame() {
+        log("starting new game...");
+    }
+
+    function wireEventHandlers() {
+        $("nav .new-game").click(handleStartNewGame);
+    }
+
+    function log(value){
+        console.log(value);
+        $ui.devLog.append(value);
+    }
+
+    function uiFactory() {
+        return {
+            devLog: $(".developer-panel .log-output")
+        };
+    };
+
     $().ready(function() {
-        $("nav .new-game").click(function(){
-            alert("new game");
-        });
+        wireEventHandlers();
     });
 
 })($);
