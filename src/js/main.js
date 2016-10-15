98e9b8cffb3588f0dfd8cff485f80b3adb8ddf5b
Zach Shewmaker <zach@azaia.net>
2016-10-14 19:29:19 -0700

    Adding turn to game.

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,30 @@
 (function($){
 
     var $ui = uiFactory();
+    var $current_game = gameFactory();
 
     function handleStartNewGame() {
+        if (!confirm("Are you sure you wish to start a new game?")) {
+            return;
+        }
+        $current_game = $current_game.getNewGame();
+        $ui.dayCounters.text($current_game.getTurn());
         log("Starting new game...", "important");
     }
 
+    function handleSkipTurn(){
+        $current_game.appendTurn();
+        $ui.dayCounters.text($current_game.getTurn());
+    }
+
+    function handleGameWon() {
+        log("You win, congrats!", "important");
+    }
+
     function wireEventHandlers() {
-        $("nav .new-game").click(handleStartNewGame);
+        $(".action-new-game").click(() => profile(handleStartNewGame));
+        $(".action-skip-turn").click(() => profile(handleSkipTurn));
+        $(".action-go-home").click(() => profile(handleGameWon));
     }
 
     function log(value, level){
@@ -30,8 +47,19 @@
 
     function uiFactory() {
         var devLog = $(".developer-panel .log-output");
+        var dayCounters = $(".day-counter");
         return {
+            dayCounters: dayCounters,
             devLog: devLog
+        };
+    };
+
+    function gameFactory() {
+        var turn = 1;
+        return {
+            appendTurn: () => turn++,
+            getTurn: () => turn,
+            getNewGame: () => gameFactory()
         };
     };
 
@@ -39,7 +67,7 @@
         profile(wireEventHandlers);
     }
 
-    $().ready(function() {
+    $().ready(() => {
         profile(documentReady);
     });
 
